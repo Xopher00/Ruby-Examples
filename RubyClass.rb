676ddb4adc1303237850a93cd739c56e84e09626
Kristopher Price <kprice5@gulls.salisbury.edu>
2018-07-17 16:14:00 +0000

    RubyClass.rb created online with Bitbucket

---
+++ RubyClass.rb
@@ -0,0 +1,22 @@
+class Computer
+  @@users = {}
+  
+  def initialize(username, password)
+  	@username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "the file #{filename} was created at #{time}."
+  end
+  
+  def Computer.get_users
+    return @@users
+  end
+end
+
+my_computer = Computer.new("Xopher", "Ryken")
